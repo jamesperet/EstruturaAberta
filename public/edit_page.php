1e5e5e167e1875b77f54155abaa6e2e87758a637
James Peret <james.peret@gmail.com>
2012-08-08 13:47:11 -0300

    Página de edição
    Criação da página "edit_page.php". Essa pagina vai ser a ferramenta
    principal do sistema onde se cria e edita outras páginas.

---
+++ public/edit_page.php
@@ -0,0 +1,135 @@
+<?php
+	require_once("../includes/initialize.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Editar Página</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    
+    <?php
+    if(!$_GET['file']){
+      echo '<link href="css/bootstrap.css" rel="stylesheet">';
+    } else {
+	  echo '<link href="../css/bootstrap.css" rel="stylesheet">';
+    }
+    ?>
+    
+    
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    
+    <?php
+    if(!$_GET['file']){
+      echo '<link href="css/bootstrap-responsive.css" rel="stylesheet">';
+    } else {
+	  echo '<link href="../css/bootstrap-responsive.css" rel="stylesheet">';
+    }
+    ?>
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="index.php">Estrutura Aberta</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="index.php">Home</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+    <div class="row">
+    	<div class="span12">
+			<form class="well">
+				<fieldset>
+					<legend>Editar página</legend>
+					
+					<div class="control-group">
+			            <label class="control-label" for="input01">Nome da Página</label>
+			            <div class="controls">
+			              <input type="text" class="input-xlarge" id="input01">
+			            </div>
+			        </div>
+
+					
+					<div class="control-group">
+						<label>Conteúdo</label>
+						<textarea class="" id="textarea" rows="12" style="width: 99%;"></textarea>
+					</div>
+
+		
+					<div class="form-actions">
+		            	<button type="submit" class="btn btn-primary">Salvar</button>
+		            	<button class="btn">Cancelar</button>
+		            </div>
+		            
+				</fieldset>
+			  
+
+			  
+			  
+			</form>
+    	</div>
+    </div>
+
+
+      <hr>
+
+      <footer>
+        <p>&copy; High Effects 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
