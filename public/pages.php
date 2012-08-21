e2a51e6f17755891c803809c0cd92715c312818b
James Peret <james.peret@gmail.com>
2012-08-21 19:31:36 -0300

    Lista de páginas
    Página php que monta uma tabela listando todas as páginas cadastradas
    no sistema junto com algumas informações como autor e a quanto tempo
    foi criada.

---
+++ public/pages.php
@@ -0,0 +1,131 @@
+<?php
+	require_once("../includes/initialize.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>
+	    Lista de páginas
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    
+    <link href="css/bootstrap.css" rel="stylesheet">
+    
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="index.php"><?php $settings = Setting::load(); echo $settings->sys_name; ?></a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class=""><a href="pages.php">Páginas</a></li>
+              <li class=""><a href="users.php">Usuários</a></li>
+            </ul>
+            <ul class="nav pull-right">
+			  <?php
+			  if($user) {
+			  	echo '<li class="divider-vertical"></li><li class="dropdown">';
+			    echo '<a href="#" class="dropdown-toggle" data-toggle="dropdown">' . $user->full_name() . ' <b class="caret"></b></a>';
+			    echo '<ul class="dropdown-menu">';
+				echo '<li><a href="user_settings.php">Minhas configurações</a></li>';
+			    echo '<li><a href="process.php?file=' . $_GET['file'] . '&action=logout">Sair</a></li>';
+			    echo '</ul></li>';
+			  } else {
+			  	  echo '<li class=""><a href="signup.php">Cadastro</a></li>';
+				  echo '<li class="active"><a href="login.php">Entrar</a></li>';
+			  }
+
+			  ?>
+			</ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+		<div class="row">
+			<table class="table table-bordered">
+			  <tbody>
+			    <thead>
+				    <tr>
+				      <th>Página</th>
+				      <th>Autor</th>
+				      <th>data</th>
+				    </tr>
+				</thead>
+					<?php
+						$pages = Page::find_all();
+							foreach($pages as $page) {
+							$user = User::find_by_id($page->creator_id);
+								echo '<tr><td><a href="' . $page->name . '/">' . $page->name . '</a></td><td>' . $user->full_name() .'</td><td>' . getElapsedTime($page->creation_date) .'</tr>';
+							}
+					?>
+			    
+			  </tbody>
+			</table>
+
+		</div>
+
+
+
+      <hr>
+
+      <footer>
+        <p>&copy; High Effects 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
